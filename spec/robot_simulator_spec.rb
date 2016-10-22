54646446423f0ebdeab4047c6397a5467d013d76
Juan <groteck@gmail.com>
2016-10-22 12:11:18 +0000

    First commit

---
+++ spec/robot_simulator_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe RobotSimulator do
+  it 'has a version number' do
+    expect(RobotSimulator::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
